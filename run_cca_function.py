fe62b92758f66eebcaa30d4f5f6e6862cea3477a
gp-lab-00 <98982501+gp-lab-00@users.noreply.github.com>
2022-04-08 13:48:04 -0400

    add files for permuation with ukbb data and parallelize

---
+++ run_cca_function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+from sklearn.cross_decomposition import CCA
+from sklearn.utils import resample
+from sklearn.utils import shuffle
+import numpy as np
+import random
+from sklearn.preprocessing import StandardScaler
+
+def run_shuffled_cca(ddata,vdata,n_cca_components,iter):
+    demographics_shuffled, vertexes_shuffled = shuffle(ddata, vdata, random_state=iter)
+    #vertexes_shuffled = shuffle(vertexes)
+    cca_model_shuffled = CCA(n_components=n_cca_components)
+    cca_model_shuffled.fit(ddata,vertexes_shuffled)
+    #coefficients_temp = cca_model_shuffled.coef_
+    #X_loadings_temp = cca_model_shuffled.x_loadings_
+    #Y_loadings_temp = cca_model_shuffled.y_loadings_
+    test1_c, test2_c = cca_model_shuffled.transform(ddata, vertexes_shuffled)
+    testcorrs = np.corrcoef(test1_c.T, test2_c.T).diagonal(offset=cca_model_shuffled.n_components)
+    return testcorrs
