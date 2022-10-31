2a824023be574f0bfd73865b1617543be57a7952
gp-lab-00 <98982501+gp-lab-00@users.noreply.github.com>
2022-10-31 10:22:57 -0400

    add explanatory comments

--- run_cca_function.py
+++ run_cca_function.py
@@ -12,13 +12,15 @@ import random
 from sklearn.preprocessing import StandardScaler
 
 def run_shuffled_cca(ddata,vdata,n_cca_components,iter):
+    
+    #Breaks links in dataset
     demographics_shuffled, vertexes_shuffled = shuffle(ddata, vdata, random_state=iter)
-    #vertexes_shuffled = shuffle(vertexes)
+    
+    #Create and run CCA model
     cca_model_shuffled = CCA(n_components=n_cca_components)
     cca_model_shuffled.fit(ddata,vertexes_shuffled)
-    #coefficients_temp = cca_model_shuffled.coef_
-    #X_loadings_temp = cca_model_shuffled.x_loadings_
-    #Y_loadings_temp = cca_model_shuffled.y_loadings_
+    
+    #Calculate and return the correlation coefficient
     test1_c, test2_c = cca_model_shuffled.transform(ddata, vertexes_shuffled)
     testcorrs = np.corrcoef(test1_c.T, test2_c.T).diagonal(offset=cca_model_shuffled.n_components)
     return testcorrs
